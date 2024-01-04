10f838d52e266d4ae4d4c652d6b88554ef29dbbf
noela005 <nehioma@gmail.com>
2024-01-04 00:15:52 -0700

    new commit

---
+++ Js/index.js
@@ -0,0 +1,13 @@
+// let navlinks = document.querySelectorAll(".nav-link")
+
+
+// for (let index = 0; index < navlinks.length; index++) {
+//     const link = navlinks[index];
+//     link.addEventListener('click', function(){
+//         console.log(link.classList)
+//         link.classList.add('active')
+//         console.log(link.classList)
+//     });
+
+    
+// }
